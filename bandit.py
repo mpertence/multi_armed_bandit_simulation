a3089507f19fbf823c8eba03813e5db2900ca2a5
mpertence <mtspertence@gmail.com>
2024-02-05 23:02:42 -0300

    ajustando classes

--- bandit.py
+++ bandit.py
@@ -1,29 +1,20 @@
 import numpy as np 
 
 class Bandit: 
-  def __init__(self, strategy):
-    self.arms = []
-    self.strategy = strategy
+    def __init__(self, strategy):
+        self.arms = []
+        self.strategy = strategy
 
-  def add_arm(self,new_arm):
-    self.arms.append(new_arm)
-    
-  def reset_arms(self):
-    for arm in self.arms:
-        arm.reset()
-        
-  def get_total_reward(self):
-    total_reward = 0
-    for arm in self.arms:
-        total_reward += arm.total_reward
-    return total_reward
+    def get_best_arm(self):
+        best_arm_index = np.argmax(self.arms)
+        best_arm_instance = self.arms[best_arm_index]
+        return best_arm_instance
 
-  def get_best_arm(self):
-    best_arm_index = np.argmax(self.arms)
-    best_arm_instance = self.arms[best_arm_index]
-    return best_arm_instance
+    def select_arm_to_run(self, arms):
+        arm = self.strategy.choose(arms)
+        return arm
 
-  def play(self):
-    arm = self.strategy.choose(self.arms)
-    arm.run()
+    def play(self, arms):
+        arm = select_arm_to_run()
+        arm.run()
 
--- multi_armed_bandit.py
+++ multi_armed_bandit.py
@@ -3,35 +3,41 @@ from bandit import Bandit
 from strategy import EpsilonGreedyStrategy, GreedyStrategy, RandomStrategy, UCB
 
 class MaB: 
-  def __init__(self, strategy):
-    self.strategy = strategy
-    self.bandit = 0
+    def __init__(self, strategy):
+        self.bandit = Bandit(strategy)
+        self.arms = []
+        self.reward_history = []
+        self.total_reward_history = []
 
-  def create_bandit(self):
-    self.bandit = Bandit(strategy=self.strategy)
-    return self.bandit
+    def create_arm(self, name, click_prob, purchase_prob, click_reward=1, purchase_reward=3):
+        arm = Arm(name, click_prob, purchase_prob)
+        self.arms.append(arm)
 
-  def create_arm(self, name, click_prob, purchase_prob, click_reward=1, purchase_reward=3):
-    arm = Arm(name, click_prob, purchase_prob)
-    self.bandit.add_arm(arm)
-
-    
-  def reset_arms(self):
-    for arm in self.arms:
-        arm.reset()
+    def reset_arms(self):
+        for arm in self.arms:
+            arm.reset()
+            
+        self.reward_history = []
         
-  def get_total_reward(self):
-    total_reward = 0
-    for arm in self.arms:
-        total_reward += arm.total_reward
-    return total_reward
+    def get_total_reward(self):
+        total_reward = 0
+        for arm in self.arms:
+            total_reward += arm.total_reward
+        return total_reward
 
-  def get_best_arm(self):
-    best_arm_index = np.argmax(self.arms)
-    best_arm_instance = self.arms[best_arm_index]
-    return best_arm_instance
-
-  def play(self):
-    arm = self.strategy.choose(self.arms)
-    arm.run()
+    def get_arm_total_reward(self):
+        list_to_update = []
+        for arm in self.arms:
+            list_to_update.append(arm.total_reward)
+        self.reward_history.append(list_to_update)
+    
+    def play(self, number_of_steps):
+        bandit = self.bandit
+        for step in range(number_of_steps):
+            arm = bandit.select_arm_to_run(self.arms)
+            arm.run()
+            self.get_arm_total_reward()
+        
+        total_reward = self.get_total_reward()
+        self.total_reward_history.append(total_reward)
 
